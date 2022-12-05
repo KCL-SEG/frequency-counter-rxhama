8d18c92cb7b7a9eccfadd4b663c8e07164c70b88
Roland Xhama <rxhama02@gmail.com>
2022-12-05 04:10:09 +0000

    Add first solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        key = item
+        if not isinstance(key, str):
+            key = str(key)
+        if (key in frequencies):
+            dictionary[key] += 1
+        else:
+            dictionary[key] = 1
+
     return frequencies
